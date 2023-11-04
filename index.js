ca9708844884a116241a78bed1242a9daf244478
Habibur Rahman <habib.swi16@gmail.com>
2023-11-04 19:00:43 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
 
 app.use(express.json());
 app.use(cors());
@@ -17,3 +20,30 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.jamv2ck.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
