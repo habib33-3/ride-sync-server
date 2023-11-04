f36779d2c1f73951358f57ae99efbfa3d7ce7ea3
Habibur Rahman <habib.swi16@gmail.com>
2023-11-04 18:41:32 +0600

    initiated local server

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server running....");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
