1fa82064bd8854ac2d33fe9e2de3069d0bf107e6
Habibur Rahman <habib.swi16@gmail.com>
2023-11-08 13:37:12 +0600

    delete api for manage services created

--- index.js
+++ index.js
@@ -129,6 +129,15 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/api/v1/deleteService/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await serviceCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
